e17dae106e0ed7defe1235c277d03b00dd34f60e
FoxInTheBoxx <ahlmalechsia@gmail.com>
2026-04-02 03:46:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from telegram.ext import (
 # =========================
 # ⚙️ НАСТРОЙКИ
 # =========================
-TOKEN = "724044213:AAHzltZdQnaSn_ou30Ohbd8v2rDtPpyuG1Q"
+TOKEN = "8724044213:AAHzltZdQnaSn_ou30Ohbd8v2rDtPpyuG1Q"
 
 ADMIN_ID = 705855212  # ← ВСТАВЬ СВОЙ ID
 ALLOWED_USERS = [ADMIN_ID]
